573cf3a9d00d90eb28e1c6807d2fcb125e28dd62
BOT22345 <nmongia48@gmail.com>
2024-09-30 01:15:35 +0530

    added the typing animation

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ======================== typing animation ========================*/
+var typed=new Typed(".typing",{
+    strings:["","Web Designer","Web Developer","Data Analysis","Game Developer"],
+    typeSpeed:60,
+    backSpeed:20,
+    loop:true 
+});
